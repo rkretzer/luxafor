19cf75310a77db5fa166486991593d72b36b7122
kretzer <rk@converia.de>
2022-12-01 11:09:58 +0100

    make adjustments for Mac OS Ventura

--- lux.sh
+++ lux.sh
@@ -4,6 +4,7 @@
 SLEEP=5
 
 ison=0
+lastfocus="No focus"
 declare -A emojis
 
 
@@ -21,17 +22,22 @@ while [ true ]; do
   fi
 
   # Check if Slack is in a call
-  slack=$(/usr/bin/python3 lswin.py | grep -i slack | wc -l)
+  slack=$(python3 lswin.py | grep -i slack | wc -l)
   if [[ $slack -gt 1 ]]; then
     busy=1
   fi
 
+  focusonoff=$(./dnd-monterey)
   focusstatus=$(osascript ./WhichFocus.scpt)
+  if [[ "$focusonoff" == "off" ]]; then
+    focusstatus="No focus"
+  fi
+
   statuscode=$(sed 's/ //g' <<< $focusstatus)
 #echo "$statuscode Emoji: ${emojis[${statuscode}]}"
   if [[ "$focusstatus" != "No focus" ]]; then
     busy=1
-    if [[ ison -eq 0 ]]; then
+    if [[ "$focusstatus" != "$lastfocus" ]]; then
       slack snooze start 120 > /dev/null;
       slack status edit --text "Fokus: $focusstatus" --emoji :${emojis[${statuscode}]}: > /dev/null;
     fi
